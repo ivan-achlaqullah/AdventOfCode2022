b222d3f8b6be241a98ca140e59e2039661f530da
Ivan Achlaqullah <ivan.achlaqullah@outlook.com>
2022-12-01 10:44:44 +0700

    First commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string text = "Hello World!\n";
+    std::string info = "Using VSCode editor, MSVC compiler\n";
+    
+    std::cout << text << info;
+}
