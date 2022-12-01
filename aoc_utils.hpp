2da2e63ad839496c023ccc0fd75712bc3c4a5bc5
Ivan Achlaqullah <ivan.achlaqullah@outlook.com>
2022-12-01 11:22:38 +0700

    Preparation

---
+++ aoc_utils.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <regex>
+
+int test_func_regex() {
+    std::regex find("digit (\\d+)");
+    std::string text("digit 2022");
+
+    std::smatch match;
+    if (std::regex_search(text, match, find)) {
+        auto res = match[1].str();
+        return std::stoi(res);
+    }
+    return -1;
+}
--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::string text = "Hello World!\n";
-    std::string info = "Using VSCode editor, MSVC compiler\n";
-    
-    std::cout << text << info;
-}
